44c90b1d5d2309a3f14ac8efd629a9ab95094752
Paul Frazier <81392028+PxFrazier@users.noreply.github.com>
2021-12-16 13:22:22 -0500

    Update radix_sort.js

--- radix_sort.js
+++ radix_sort.js
@@ -12,22 +12,32 @@ function radixSort(input_array)
     };
 
     let maximum = MAX();
-    let iterator = maximum - 1, n = maximum - 1;
+    let iterator = maximum - 1;
     
     while(iterator >= 0)
     {
         let buckets = [...Array(255)].map(()=>[]);
         input_array.forEach(entry=>{
-            iterator < entry.length ?
+            iterator < entry.length && entry.length != 1 ?
             buckets[entry.charCodeAt(iterator)].push(entry) :
-            buckets[entry.charCodeAt(iterator - n)].push(entry);
+            buckets[entry.charCodeAt(entry.length - 1)].unshift(entry);
         });
         input_array = [].concat.apply([], buckets);
         iterator--;
-        n--;
     }
     return input_array;
 }
 
-console.log(radixSort(['dfg', 'ytr', 'jhgf', 'asd', 'aaasdf', 'z']));
-//Outputs: ['aaasdf', 'asd', 'dfg', 'jhgf', 'ytr', 'z']
+let array = ['one', 'two', 'three', 'four', '1', '2', '3', '4'];
+
+console.log(radixSort(['dfg', 'ytr', 'jhgf', 'asd', 'aaasdf', 'z', './', '/lkk..']));
+//Outputs: ['./', '/lkk..', 'aaasdf', 'asd', 'dfg', 'jhgf', 'ytr', 'z']
+
+console.log(radixSort(array));
+//Outputs: ['1', '2', '3', '4', 'four', 'one', 'three', 'two']
+
+console.log(radixSort(['cccc', 'c', 'cc', 'ccc']));
+//Outputs: ['c', 'cc', 'ccc', 'cccc']
+
+console.log(radixSort(['a', 'aasd', 'cab', 'cba', 'c', 'b', 'ffbba', 'd']));
+//Outputs: ['a', 'aasd', 'b', 'c', 'cab', 'cba', 'd', 'ffbba']
