bc9421c5d2a3642e0b4a8d70f62a96ad7fa73a0f
Paul Frazier <81392028+PxFrazier@users.noreply.github.com>
2021-12-16 09:15:47 -0500

    Add files via upload

---
+++ radix_sort.js
@@ -0,0 +1,34 @@
+let array = ['c', 'b', 'a', 'd', 'cab', 'bc', 'abb', 'fbba', 'cb', 'cba', 'aca'];
+
+function radixSort(input_array)
+{
+    const MAX = ()=>{
+        let maximum = 0;
+        input_array.forEach(entry=>{
+            if(maximum < entry.length)
+            {
+                maximum = entry.length;
+            }
+        });
+        return maximum;
+    };
+
+    let maximum = MAX();
+    let iterator = maximum - 1, n = maximum - 1;
+    
+    while(iterator >= 0)
+    {
+        let buckets = [...Array(255)].map(()=>[]);
+        input_array.forEach(entry=>{
+            iterator < entry.length ?
+            buckets[entry.charCodeAt(iterator)].push(entry) :
+            buckets[entry.charCodeAt(iterator - n)].push(entry);
+        });
+        input_array = [].concat.apply([], buckets);
+        iterator--;
+        n--;
+    }
+    return input_array;
+}
+
+console.log(radixSort(array));
